e9523915fcf74629f973d6a42b65e755257befad
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-09-15 18:53:01 +0300

    auth commented

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,25 +1,25 @@
 
-// import { AuthGuard as NestAuthGuard } from '@nestjs/passport';
+import { AuthGuard as NestAuthGuard } from '@nestjs/passport';
 
-// export const AuthGuard = NestAuthGuard('jwt');
+export const AuthGuard = NestAuthGuard('jwt');
 
 
 // the following is for test cases
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 
-@Injectable()
-export class AuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
-    const req = context.switchToHttp().getRequest();
-    const fakeUser = {
-      roles: ['ADMIN'],
-      username: '@fake',
-      id: '5f5b45b20e841b7ceadfbd62'
-    };
-    req.user = fakeUser;
-    return true;
-  }
-}
+// @Injectable()
+// export class AuthGuard implements CanActivate {
+//   canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+//     const req = context.switchToHttp().getRequest();
+//     const fakeUser = {
+//       roles: ['ADMIN'],
+//       username: '@fake',
+//       id: '5f5b45b20e841b7ceadfbd62'
+//     };
+//     req.user = fakeUser;
+//     return true;
+//   }
+// }
 
 
 
