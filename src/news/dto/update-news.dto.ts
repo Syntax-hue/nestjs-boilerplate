ea567ccb8f781d93416e858b32d22e65c3173ee0
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-09-16 19:55:19 +0300

    update news dto

---
+++ src/news/dto/update-news.dto.ts
@@ -0,0 +1,28 @@
+import { IsArray, IsOptional, IsString, MaxLength, MinLength, IsNotEmpty } from "class-validator";
+
+export class UpdateNewsDto {
+    @IsOptional()
+    @IsString()
+    @MaxLength(100)
+    @MinLength(5)
+    public readonly title;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    @MinLength(5)
+    public readonly shortDescription;
+
+    @IsOptional()
+    @IsString()
+    @IsArray()
+    public readonly tags;
+
+    @IsOptional()
+    @IsString()
+    public readonly frontImage;
+
+    @IsOptional()
+    @IsString()
+    public readonly html;
+}
