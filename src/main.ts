38984faafc5a98b38bbcead5517bd3e7be8e075e
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-09-21 20:33:24 +0300

    create admin function api and setting swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true
   }))
+
+  const options = new DocumentBuilder()
+    .setTitle('Boilerplate')
+    .setDescription('Boilerplate application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
